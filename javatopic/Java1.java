b4ea39c77fc175c898019e924f60c86e37a77cde
sweta patel <sweta_0279@yahoo.co.uk>
2023-02-14 21:05:48 +0000

    practise

---
+++ javatopic/Java1.java
@@ -0,0 +1,17 @@
+package javatopic;
+
+public class Java1 {
+
+    public static void main(String[] args) {
+        /*
+        I have refactored java package to java topic
+         */
+        System.out.println("1: Java Package");
+        System.out.println("2: Java Class");
+        System.out.println("3: Variables");
+        System.out.println("4: Single line comment");
+        System.out.println("5: Github push");
+
+    }
+
+}
---
+++ manualtopic/Manual1.java
@@ -0,0 +1,22 @@
+package manualtopic;
+
+public class Manual1 {
+
+    public static void main(String[] args) {
+        //I changed the name of manual package from manual to manualtopic
+        // I am creating 10 topics of manual testing
+
+        System.out.println("1: Static Testing");
+        System.out.println("2: Dynamic Testing");
+        System.out.println("3: Unit Testing");
+        System.out.println("4: Integration Testing");
+        System.out.println("5: System Testing");
+        System.out.println("6: Acceptance Testing");
+        System.out.println("7: Smoke Testing");
+        System.out.println("8: Sanity Testing");
+        System.out.println("9: Explorative Testing");
+        System.out.println("10: Regression Testing");
+
+
+    }
+}
